c5ee16fb574053872d05934c38da2fdfb1d01ad9
matthieu <matthieu.schmidt@epfl.ch>
2022-01-17 16:42:47 +0100

    zoom & basic live view

--- QontrollerMainWindow.py
+++ QontrollerMainWindow.py
@@ -1,3 +1,5 @@
+import time
+
 from PyQt5 import QtCore, QtGui, QtWidgets
 import os
 import qontroller
@@ -8,16 +10,46 @@ from device import Device
 from picam_settings import PicamSettings
 
 
+class Worker(QtCore.QObject):
+
+    finished = QtCore.pyqtSignal()  # give worker class a finished signal
+
+    def __init__(self, main_window, parent=None):
+        QtCore.QObject.__init__(self, parent=parent)
+        self.continue_run = True  # provide a bool run condition for the class
+        self.main_window = main_window
+
+    def do_work(self):
+        i = 1
+        while self.continue_run:  # give the loop a stoppable condition
+            print(i)
+            #QtCore.QThread.sleep(1)
+            self.main_window.refresh_view()
+            i = i + 1
+        self.finished.emit()  # emit the finished signal when the loop is done
+
+    def stop(self):
+        self.continue_run = False  # set the run condition to false on stop
+
+
 class QontrollerMainWindow(QtWidgets.QMainWindow, qontroller.Ui_MainWindow):
+    stop_signal = QtCore.pyqtSignal()
+
     def __init__(self, parent=None):
         super(QontrollerMainWindow, self).__init__(parent)
         self.setupUi(self)
+
+        self.resize(QtWidgets.QDesktopWidget().availableGeometry(self).size() * 0.8)
 
         # Attributes
 
         self.host_list = []
         self.currentDeviceID = None
-        self.currentDevice = None
+
+        self.full_pixmap = None
+        self.zoom_value = self.sliderZoom.value()
+
+        self.do_auto_refresh = self.btnLiveView.isChecked()
 
         self.comboTimeoutUnit.addItems(["Seconds", "Minutes", "Hours"])
 
@@ -27,10 +59,31 @@ class QontrollerMainWindow(QtWidgets.QMainWindow, qontroller.Ui_MainWindow):
         self.btnRescanDevices.clicked.connect(self.scan_devices)
 
         self.listBoxDevices.currentItemChanged.connect(self.current_item_changed)
+        self.sliderZoom.valueChanged.connect(self.zoom)
 
         # Execute on startup
 
         self.scan_devices()
+
+        # Thread:
+
+        self.thread = QtCore.QThread()
+        self.worker = Worker(self)
+        self.stop_signal.connect(self.worker.stop)  # connect stop signal to worker stop method
+        self.worker.moveToThread(self.thread)
+
+        self.worker.finished.connect(self.thread.quit)  # connect the workers finished signal to stop thread
+        # self.worker.finished.connect(self.worker.deleteLater)  # connect the workers finished signal to clean up worker
+        # self.thread.finished.connect(self.thread.deleteLater)  # connect threads finished signal to clean up thread
+
+        self.thread.started.connect(self.worker.do_work)
+        self.thread.finished.connect(self.worker.stop)
+
+
+        # When stop_btn is clicked this runs. Terminates the worker and the thread.
+
+    def stop_thread(self):
+        self.stop_signal.emit()  # emit the finished signal on stop
 
 
 
@@ -64,9 +117,17 @@ class QontrollerMainWindow(QtWidgets.QMainWindow, qontroller.Ui_MainWindow):
         if self.currentDeviceID is not None:
             currentDevice = self.host_list[self.currentDeviceID]
             s = PicamSettings(self)
-            self.labelDisplay.setPixmap(currentDevice.get_frame(s))
+            self.full_pixmap = currentDevice.get_frame(s)
+            # self.labelDisplay.setPixmap(self.full_pixmap)
+            self.labelDisplay.setPixmap(self.full_pixmap.scaled(self.full_pixmap.size() * self.sliderZoom.value() / 100,
+                                                                QtCore.Qt.KeepAspectRatio))
         else:
             print("Please first select a device")
+
+    def auto_refresh(self):
+        while self.do_auto_refresh:
+            print("yo")
+            QtCore.QThread.sleep(1)
 
     def on_listBoxDevices_clicked(self, index):
         pass
@@ -80,6 +141,10 @@ class QontrollerMainWindow(QtWidgets.QMainWindow, qontroller.Ui_MainWindow):
                 devices_marked_for_recording.append(self.host_list[i])
 
         return devices_marked_for_recording
+
+    def zoom(self):
+        self.labelDisplay.setPixmap(self.full_pixmap.scaled(self.full_pixmap.size()*self.sliderZoom.value()/100, QtCore.Qt.KeepAspectRatio))
+
 
     @QtCore.pyqtSlot(QtWidgets.QListWidgetItem, QtWidgets.QListWidgetItem)
     def current_item_changed(self, d0,d1):
@@ -154,3 +219,34 @@ class QontrollerMainWindow(QtWidgets.QMainWindow, qontroller.Ui_MainWindow):
             if d.is_running:
                 running_list.append(d)
         return running_list
+
+
+    @QtCore.pyqtSlot()
+    def on_btnFitView_clicked(self):
+        pxm = self.labelDisplay.pixmap()
+        self.labelDisplay.setPixmap(self.scaled(self.scrollArea.size(), QtCore.Qt.KeepAspectRatio))
+
+    @QtCore.pyqtSlot()
+    def on_btnOriginalView_clicked(self):
+        self.labelDisplay.setPixmap(self.full_pixmap)
+
+    @QtCore.pyqtSlot(bool)
+    def on_btnLiveView_clicked(self, checked):
+        print(checked)
+        self.do_auto_refresh = checked
+        if checked:
+            self.thread = QtCore.QThread()
+            self.worker = Worker(self)
+            self.stop_signal.connect(self.worker.stop)  # connect stop signal to worker stop method
+            self.worker.moveToThread(self.thread)
+
+            self.worker.finished.connect(self.thread.quit)  # connect the workers finished signal to stop thread
+            self.worker.finished.connect(self.worker.deleteLater)  # connect the workers finished signal to clean up worker
+            self.thread.finished.connect(self.thread.deleteLater)  # connect threads finished signal to clean up thread
+
+            self.thread.started.connect(self.worker.do_work)
+            self.thread.finished.connect(self.worker.stop)
+            # self.auto_refresh()
+            self.thread.start()
+        else:
+            self.stop_thread()
